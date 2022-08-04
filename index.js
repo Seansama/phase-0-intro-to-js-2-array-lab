edce87ae2ce9726bddc67927a5136710428c026b
ShaunM <seansama__@pop-os.localdomain>
2022-08-04 19:16:01 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+cats[3] = "Ralph";
+
+}
+
+function  destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop([2]);
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift([0]);
+}
+
+function appendCat(name){
+
+    
+    let cats = ['Milo', 'Otis', 'Garfield', 'Broom']
+    return cats;
+}
+
+function prependCat(name){
+
+let cats = ["Milo", "Otis", "Garfield"]
+cats.unshift('Arnold');
+return cats;
+}
+
+function removeLastCat(){
+    let cats = ["Milo", "Otis", "Garfield"]
+    cats.pop([2]);
+    return cats;
+}
+
+function removeFirstCat(){
+    let cats = ["Milo", "Otis", "Garfield"]
+    cats.shift([0]);
+    return cats;
+}
